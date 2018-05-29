04623c4c58abf77d91d84ef0ca845fb2560b88a8
Jason Lipowicz <jason.lipowicz16@imperial.ac.uk>
2018-05-29 15:17:38 +0100

    Add tests for login

--- tests/Unit/LoginTest.php
+++ tests/Unit/LoginTest.php
@@ -9,9 +9,57 @@ use App\User;
 
 class LoginTest extends TestCase
 {
-  
-    public function testForInvalidDetails()
+    use RefreshDatabase;
+
+    public function testLoginFormDisplayed()
     {
-      // TODO
+      $response = $this->get('/login');
+      $response->assertStatus(200);
+    }
+
+    public function testForValidUser()
+    {
+      $user = factory(User::class)->create([
+        'email' => 'john@example.com', 
+        'password' => Hash::make('testpass123')
+      ]);
+
+      $response = $this->post('/login', [
+        'email' => $user->email,
+        'password' => 'testpass123'
+      ]);
+
+      $response->assertStatus(302);
+      $response->assertRedirect('/dashboard');      
+      $this->assertAuthenticatedAs($user);
+    }
+
+    public function testForInvalidUser()
+    {
+        $user = factory(User::class)->create();
+        session()->setPreviousUrl('/login');
+        $response = $this->post('/login', [
+            'email' => $user->email,
+            'password' => 'invalid'
+        ]);
+        $response->assertRedirect('/login');
+        $this->assertGuest();
+    }
+    
+    public function testLogoutAnAuthenticatedUser()
+    {
+        $user = factory(User::class)->create();
+        $response = $this->actingAs($user)->get('/logout');
+        $response->assertStatus(302);
+        $response->assertRedirect('/');
+        $this->assertGuest();
+    }
+
+    public function testRedirectToLoginIfNotAuthenticated()
+    {
+        $response = $this->get('/dashboard');
+        $response->assertStatus(302);
+        $response->assertRedirect('/login');
+        $this->assertGuest();
     }
 }
