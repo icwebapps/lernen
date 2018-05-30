395b05d7da95e53821961322ec7e223e2436efa9
Jason Lipowicz <jason.lipowicz16@imperial.ac.uk>
2018-05-30 12:09:05 +0100

    Added css and templating for login

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html>
+  <head>
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,800" rel="stylesheet">
+    <link rel="stylesheet" href="/css/style.css" />
+    <title>@yield('title') | Lernen</title>
+  </head>
+  <body>
+    <div class="site">
+      @yield('content')
+    </div>
+  </body>
+</html>
