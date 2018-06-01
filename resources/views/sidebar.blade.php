d6b5b96b2a9b6f15600018e68db2f816d40a8328
Jason Lipowicz <jason.lipowicz16@imperial.ac.uk>
2018-06-01 14:58:07 +0100

    Highlight correct page in sidebar

--- resources/views/sidebar.blade.php
+++ resources/views/sidebar.blade.php
@@ -1,16 +1,16 @@
 <div class="sidebar">
   <img class="logo-icon" src="/images/icon.png" />
   <div class="nav">
-    <div class="nav-item">
+    <div class="nav-item @if ($selected == "dashboard") nav-selected @endif">
       <a href="/dashboard"><img src="/images/icons8-dashboard-50.png" /></a>
     </div>
-    <div class="nav-item nav-selected">
+    <div class="nav-item @if ($selected == "calendar") nav-selected @endif">
       <a href="/calendar"><img src="/images/icons8-today-100.png" /></a>
     </div>
-    <div class="nav-item">
+    <div class="nav-item @if ($selected == "contacts") nav-selected @endif">
       <a href="/students"><img src="/images/icons8-address-book-2-filled-100.png" /></a>
     </div>
-    <div class="nav-item">
+    <div class="nav-item @if ($selected == "account") nav-selected @endif">
       <a href="/account"><img src="/images/icons8-male-user-50.png" /></a>
     </div>
   </div>
--- resources/views/tutees.blade.php
+++ resources/views/tutees.blade.php
@@ -4,7 +4,7 @@
 
 @section('content')
 
-@component('sidebar')
+@component('sidebar', ['selected' => 'contacts'])
 @endcomponent
 
 <div class="main">
