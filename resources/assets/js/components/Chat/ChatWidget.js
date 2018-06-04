6cbeeb70bda310e90260262a5ea81688bdf1e89e
Jason Lipowicz <jason.lipowicz16@imperial.ac.uk>
2018-06-05 00:10:01 +0100

    Add Pusher and Curl to project

--- resources/assets/js/components/Chat/ChatWidget.js
+++ resources/assets/js/components/Chat/ChatWidget.js
@@ -5,19 +5,40 @@ export default class ChatWidget extends Component {
   constructor(props) {
     super();
     this.state = {
-      messages: []
+      messages: [],
+      text: ''
     }
   }
 
+  componentDidMount() {
+    Echo.private('chat')
+      .listen('MessageSent', (e) => {
+        this.setState({ messages: [ ...this.state.messages, { message: e.message.message, user: e.user }] })
+      });
+  }
+
+  onChange(e) {
+    this.setState({ text: e.target.value });
+  }
+
+  fetchMessages() {
+
+  }
+
+  sendMessage() {
+
+  }
+
   render() {
-    return (
+    return ([
       <ul>
         {
           this.state.messages.map((m, i) =>
             <li key={i}>{m.outgoing ? <b>{m.message}</b> : m.message}</li>
           )
         }
-      </ul>
-    )
+      </ul>,
+      <textarea placeholder="Enter message" onChange={e=>this.onChange(e)} value={this.state.text}></textarea>
+    ])
   }
 }
