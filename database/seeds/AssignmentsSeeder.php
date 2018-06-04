0ace4a5545ef7e762c2a6c5fc4176e3d43ced29e
Alexander Zakon <azakon1997@gmail.com>
2018-06-04 20:09:58 +0100

    Revert "Create assignment seeder"
    This reverts commit 972c09446dbcc022aebc86b00b648d3f3c7daa41.

--- database/seeds/AssignmentsSeeder.php
+++ database/seeds/AssignmentsSeeder.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Database\Seeder;
 use App\Assignment;
+use App\Lesson;
+use App\Student;
 use App\Tutor;
 use App\User;
 
@@ -22,9 +24,8 @@ class AssignmentsSeeder extends Seeder
                 'student_id' => 1,
                 'tutor_id' => $t->user_id,
                 'subject' =>str_random(10),
-                'date_set' => date("Y-m-d", strtotime("-2 days")),
-                'date_due' => date("Y-m-d", strtotime("+7 days")),
-                'rec'
+                'date_set' => date("T-m-d", strtottime("-1 days")),
+                'date_due' => date("Y-m-d", strtotime("+7 days"))
             ]);
         }
     }
