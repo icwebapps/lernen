5286e8cc9b612aa5ab5184e1eebbf1dcd37317c8
Jason Lipowicz <jason.lipowicz16@imperial.ac.uk>
2018-06-04 16:14:18 +0100

    Fix React error by assigning key to child

--- resources/assets/js/components/Calendar/CalendarCell.js
+++ resources/assets/js/components/Calendar/CalendarCell.js
@@ -9,8 +9,8 @@ export default class CalendarCell extends Component {
         { this.props.events.length > 0 ?
           <div className="calendar-events">
             {
-              this.props.events.map(e =>
-                <div className="event-item" key={e}>
+              this.props.events.map((e, i) =>
+                <div className="event-item" key={'event'+i}>
                   <a className="key-colour accent-red"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50">
                   <path d="M25,48C12.318,48,2,37.682,2,25S12.318,2,25,2s23,10.318,23,23S37.682,48,25,48z"></path>
                   </svg></a>
