cfeacb45fce739349428d269383c5972ea3c26a9
Boaz Francis <boaz.francis16@imperial.ac.uk>
2018-06-04 19:04:12 +0100

    Add migration to add tutor_id to resources table

---
+++ database/migrations/2018_06_04_175124_add_tutor_id_to_resources_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddTutorIdToResourcesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('resources', function (Blueprint $table) {
+            $table->integer('tutor_id')->unsigned();
+            $table->foreign('tutor_id')->references('user_id')->on('tutors');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+      Schema::dropIfExists('resources');
+    }
+}
