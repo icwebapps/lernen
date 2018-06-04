7c58b5e981d4b99b4bc9a94394e4d3c02c9736e9
Alexander Zakon <azakon1997@gmail.com>
2018-06-04 18:06:55 +0100

    Modify assigments migration and create assignment seeder

---
+++ app/Assignment.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+
+
+use Illuminate\Database\Eloquent\Model;
+
+class Assignment extends Model
+{
+    public function student() {
+        return $this->belongsTo('App\Student', 'student_id', 'user_id');
+    }
+    public function tutor() {
+        return $this->belongsTo('App\Tutor', 'tutor_id', 'user_id');
+    }
+}
---
+++ database/seeds/AssignmentsSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Assignment;
+use App\Lesson;
+use App\Student;
+use App\Tutor;
+use App\User;
+
+class AssignmentsSeeder extends Seeder
+{
+
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // Create assignments from 5 different tutors for student 1
+        for ($i=0; $i<5; $i++) {
+            $t = Tutor::create([ 'user_id' => factory(User::class)->create()->id]);
+            Assignment::create([
+                'student_id' => 1,
+                'tutor_id' => $t->user_id,
+                'subject' =>str_random(10),
+                'date_set' => date("T-m-d", strtottime("-1 days")),
+                'date_due' => date("Y-m-d", strtotime("+7 days"))
+            ]);
+        }
+    }
+}
