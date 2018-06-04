691681f16529f837bae931681662b8d1d46dac05
Shravan Nageswaran <shravannageswaran@dyn3200-14.wlan.ic.ac.uk>
2018-06-04 17:36:19 +0100

    Added header component in upcoming

--- resources/views/upcoming.blade.php
+++ resources/views/upcoming.blade.php
@@ -4,25 +4,17 @@
 
 @section('content')
 
-@component('sidebar')
+@component('sidebar', ['selected' => 'calendar'])
 @endcomponent
 
 <div class="main">
-        <div class="header">
-          <div class="header-left">
-            <div class="page-title">Schedule</div>
-            <div class="header-tabs">
-              <div class="header-tab-item tab-selected">Upcoming</div>
-              <div class="header-tab-item">Calendar</div>
-            </div>
-          </div>
-          <div class="header-center">
-            <img src="images/logo.png" class="logo" />
-          </div>
-          <div class="header-right">
-            <div class="header-logout"><img src="images/icons8-shutdown-50.png" /></div>
-          </div>
-        </div>
+
+  @component('header')
+    @slot('title') Schedule @endslot
+      <div class="header-tab-item tab-selected">Upcoming</div>
+      <div class="header-tab-item">Calendar</div>
+   @endcomponent
+
         <div class="width-scrollable">
           <div class="column">
             <div class="column-title">Today</div>
