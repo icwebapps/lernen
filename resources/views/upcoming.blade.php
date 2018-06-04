ec7ae3beb4fc461616073766011fac8b17ef897e
Shravan Nageswaran <shravannageswaran@dyn3200-14.wlan.ic.ac.uk>
2018-06-04 17:23:57 +0100

    Deleted menu file to eliminate error in header.

--- resources/views/upcoming.blade.php
+++ resources/views/upcoming.blade.php
@@ -7,9 +7,22 @@
 @component('sidebar')
 @endcomponent
 
-@component('menu')
-@endcomponent
-
+<div class="main">
+        <div class="header">
+          <div class="header-left">
+            <div class="page-title">Schedule</div>
+            <div class="header-tabs">
+              <div class="header-tab-item tab-selected">Upcoming</div>
+              <div class="header-tab-item">Calendar</div>
+            </div>
+          </div>
+          <div class="header-center">
+            <img src="images/logo.png" class="logo" />
+          </div>
+          <div class="header-right">
+            <div class="header-logout"><img src="images/icons8-shutdown-50.png" /></div>
+          </div>
+        </div>
         <div class="width-scrollable">
           <div class="column">
             <div class="column-title">Today</div>
