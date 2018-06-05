b0fefb0d40df1708221a12324b74dc3c6bb7fa75
Boaz Francis <boaz.francis16@imperial.ac.uk>
2018-06-05 11:36:27 +0100

    Define Resource model

--- app/Resource.php
+++ app/Resource.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Resource extends Model
 {
-  
+  public function Resource() {
+    return $this->belongsTo('App\Tutor');
+  }
+
+  public function lessons() {
+    return $this->hasMany('App\Student', 'student_id', 'tutor_id');
+  }
 }
