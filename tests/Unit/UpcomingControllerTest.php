0fa738df967a04c63d6bf188ae0408fded6d4d38
Shravan Nageswaran <shravannageswaran@dyn3204-80.wlan.ic.ac.uk>
2018-06-05 17:14:36 +0100

    Deleted duplicate test in Upcoming to mirror the controller

--- tests/Unit/UpcomingControllerTest.php
+++ tests/Unit/UpcomingControllerTest.php
@@ -17,22 +17,4 @@ class UpcomingControllerTest extends TestCase
     $response->assertStatus(200);
   }  
 
-  public function testRetrieveEvents()
-  {
-    $user = factory(User::class)->create();
-    $tutor = factory(Tutor::class)->create([
-      'user_id' => $user->id
-    ]);
-    $student = factory(Student::class)->create();
-    $lesson = factory(Lesson::class)->create([
-      'tutor_id' => $tutor->user_id,
-      'student_id' => $student->user_id,
-      'date' => date("Y-m-d", strtotime("+7 days")),
-    ]);
-
-    $response = $this->actingAs($user)->get('/calendar/events');
-    $response->assertStatus(200);
-    $response->assertJson([ 'events' => [] ]);
-  }
-
 }
