13b392612b95765273652ce7db2b63a238ecb698
Boaz Francis <boaz.francis16@imperial.ac.uk>
2018-06-05 13:43:36 +0100

    Create seeder for resources

--- database/seeds/ResourcesSeeder.php
+++ database/seeds/ResourcesSeeder.php
@@ -26,5 +26,18 @@ class ResourcesSeeder extends Seeder
       'created_at' => date("Y-m-d", strtotime("+7 days")),
       ]);
     }
+
+    for ($i=0; $i<15; $i++) {
+      $s = Student::create([ 'user_id' => factory(User::class)->create()->id ]);
+      Resource::create([
+      'name' => str_random(8),
+      'url' => str_random(10),
+      'tutor_id' => 2,
+      'student_id' => $s->user_id,
+      'created_at' => date("Y-m-d", strtotime("+7 days")),
+      'created_at' => date("Y-m-d", strtotime("+7 days")),
+      ]);
+    }
   }
+
 }
