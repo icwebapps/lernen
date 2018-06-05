3892b91ba2e151fd70de6a4d3ce41c966930a144
Boaz Francis <boaz.francis16@imperial.ac.uk>
2018-06-05 11:37:20 +0100

    Add seeder for resources and modify DatabaseSeeder to include it

--- database/seeds/ResourcesSeeder.php
+++ database/seeds/ResourcesSeeder.php
@@ -1,16 +1,30 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Student;
+use App\Resource;
+use App\User;
+
 
 class ResourcesSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        //
+  /**
+   * Run the database seeds.
+   *
+   * @return void
+   */
+  public function run()
+  {
+    for ($i=0; $i<15; $i++) {
+      $s = Student::create([ 'user_id' => factory(User::class)->create()->id ]);
+      Resource::create([
+      'name' => str_random(8),
+      'url' => str_random(10),
+      'tutor_id' => 4,
+      'student_id' => $s->user_id,
+      'created_at' => date("Y-m-d", strtotime("+7 days")),
+      'created_at' => date("Y-m-d", strtotime("+7 days")),
+      ]);
     }
+  }
 }
