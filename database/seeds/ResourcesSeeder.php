88a00d2ed6ed94ae06783c2e34578ade4941a8b2
Boaz Francis <boaz.francis16@imperial.ac.uk>
2018-06-05 16:09:42 +0100

    Create student assignments seeder

--- database/seeds/ResourcesSeeder.php
+++ database/seeds/ResourcesSeeder.php
@@ -16,24 +16,20 @@ class ResourcesSeeder extends Seeder
   public function run()
   {
     for ($i=0; $i<15; $i++) {
-      $s = Student::create([ 'user_id' => factory(User::class)->create()->id ]);
       Resource::create([
       'name' => str_random(8),
       'url' => str_random(10),
       'tutor_id' => 4,
-      'student_id' => $s->user_id,
       'created_at' => date("Y-m-d", strtotime("+7 days")),
       'created_at' => date("Y-m-d", strtotime("+7 days")),
       ]);
     }
 
     for ($i=0; $i<15; $i++) {
-      $s = Student::create([ 'user_id' => factory(User::class)->create()->id ]);
       Resource::create([
       'name' => str_random(8),
       'url' => str_random(10),
       'tutor_id' => 2,
-      'student_id' => $s->user_id,
       'created_at' => date("Y-m-d", strtotime("+7 days")),
       'created_at' => date("Y-m-d", strtotime("+7 days")),
       ]);
