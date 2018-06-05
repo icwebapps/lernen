0f4b0d198e86dde4cea5ad30df2befea31a1130e
Jason Lipowicz <jason.lipowicz16@imperial.ac.uk>
2018-06-05 20:46:28 +0100

    Temporary dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,2 +1,28 @@
-<h1>WELCOME {{ Auth::user()->name }}</h1>
-<a href="/logout">Click here to logout</a>
+@extends('layout')
+
+@section('title', 'Dashboard')
+
+@section('content')
+
+@component('sidebar', ['selected' => 'dashboard'])
+@endcomponent
+
+
+<div class="main">
+
+  <div class="header">
+    <div class="header-left">
+    <div class="page-title">Dashboard</div>
+    <div class="page-title page-dark">Welcome {{ explode(" ", Auth::user()->name)[0] }}</div>
+    </div>
+    <div class="header-center">
+    <img src="/images/logo.png" class="logo" />
+    </div>
+    <div class="header-right">
+    <div class="header-logout"><a href="/logout"><img src="/images/icons8-shutdown-50.png" /></a></div>
+    </div>
+  </div>
+
+</div>
+
+@endsection
