d8b4fc4ecb5a243dc744f9c33c68c016099a62f1
Jason Lipowicz <jason.lipowicz16@imperial.ac.uk>
2018-06-06 21:46:36 +0100

    Use pictures in Resource component

--- resources/assets/js/components/Resources/ResourcesRow.js
+++ resources/assets/js/components/Resources/ResourcesRow.js
@@ -8,9 +8,12 @@ const ResourcesRow = ({resource}) =>
     <div className="resources-table-cell">Assignment</div>
     <div className="resources-table-cell">0</div>
     <div className="resources-table-cell">{resource.created_at}</div>
-    <div className="resources-table-cell resources-faces-list"> 
-      <img src="/images/jasonlipowicz.png" />
-      <img src="/images/boazfrancis.jpg" />
+    <div className="resources-table-cell resources-faces-list">
+      {
+        resource.students.map((s, j) =>
+          <img key={"pp"+j} src={s.user.profile_picture} />
+        )
+      }
       <img src="/images/icons8-plus-50.png" />
     </div>
   </div>;
