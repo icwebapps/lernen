794c0743b6ed323ffa87e2507684138d5e153d21
Boaz Francis <boaz.francis16@imperial.ac.uk>
2018-06-06 13:05:57 +0100

    Change indentation from 4 to 2 migrations

--- database/migrations/2018_06_04_175124_add_tutor_id_to_resources_table.php
+++ database/migrations/2018_06_04_175124_add_tutor_id_to_resources_table.php
@@ -6,26 +6,26 @@ use Illuminate\Database\Migrations\Migration;
 
 class AddTutorIdToResourcesTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::table('resources', function (Blueprint $table) {
-            $table->integer('tutor_id')->unsigned();
-            $table->foreign('tutor_id')->references('user_id')->on('tutors');
-        });
-    }
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+    Schema::table('resources', function (Blueprint $table) {
+      $table->integer('tutor_id')->unsigned();
+      $table->foreign('tutor_id')->references('user_id')->on('tutors');
+    });
+  }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-      Schema::dropIfExists('resources');
-    }
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down()
+  {
+    Schema::dropIfExists('resources');
+  }
 }
--- database/migrations/2018_06_05_141111_add_student_id_to_student_assignment_table.php
+++ database/migrations/2018_06_05_141111_add_student_id_to_student_assignment_table.php
@@ -6,26 +6,26 @@ use Illuminate\Database\Migrations\Migration;
 
 class AddStudentIdToStudentAssignmentTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::table('student_assignments', function (Blueprint $table) {
-            $table->integer('student_id')->unsigned();
-            $table->foreign('student_id')->references('user_id')->on('students');
-        });
-    }
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+    Schema::table('student_assignments', function (Blueprint $table) {
+      $table->integer('student_id')->unsigned();
+      $table->foreign('student_id')->references('user_id')->on('students');
+    });
+  }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-      Schema::dropIfExists('student_assignments');
-    }
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down()
+  {
+    Schema::dropIfExists('student_assignments');
+  }
 }
