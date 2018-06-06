fa1b4d416e8ecd80a6acf22188b22204ef450a9a
Jason Lipowicz <jason.lipowicz16@imperial.ac.uk>
2018-06-06 20:54:08 +0100

    Display sidebar link to resources for tutors

--- resources/views/sidebar.blade.php
+++ resources/views/sidebar.blade.php
@@ -10,6 +10,11 @@
     <div class="nav-item @if ($selected == "contacts") nav-selected @endif">
       <a href="/contacts"><img src="/images/icons8-address-book-2-filled-100.png" /></a>
     </div>
+    @if (Auth::user()->isTutor())
+    <div class="nav-item @if ($selected == "resources") nav-selected @endif">
+      <a href="/resources"><img src="/images/icons8-open-50.png" /></a>
+    </div>
+    @endif
     <div class="nav-item @if ($selected == "account") nav-selected @endif">
       <a href="/account"><img src="/images/icons8-male-user-50.png" /></a>
     </div>
