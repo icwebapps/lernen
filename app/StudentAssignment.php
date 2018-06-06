72bee572df091b55fe789e74ee89c57ab489c91f
Boaz Francis <boaz.francis16@imperial.ac.uk>
2018-06-06 13:02:47 +0100

    Change indentation from 4 to 2 student assignment model

--- app/StudentAssignment.php
+++ app/StudentAssignment.php
@@ -4,10 +4,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class StudentAssignment extends Model
 {
-    public function student() {
-        return $this->belongsTo('App\Student', 'student_id', 'user_id');
-    }
-    public function tutor() {
-        return $this->belongsTo('App\Tutor', 'tutor_id', 'user_id');
-    }
+  public function student() {
+    return $this->belongsTo('App\Student', 'student_id', 'user_id');
+  }
+  
+  public function tutor() {
+    return $this->belongsTo('App\Tutor', 'tutor_id', 'user_id');
+  }
 }
