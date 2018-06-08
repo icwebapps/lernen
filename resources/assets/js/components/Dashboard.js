8cf6cdc62c6abc42ca8229724e5085b4f9fcdab6
Alexander Zakon <azakon1997@gmail.com>
2018-06-08 12:14:49 +0100

    Set up file layout for student dashboard

---
+++ resources/assets/js/components/Dashboard.js
@@ -0,0 +1,16 @@
+import ReactDOM from "react-dom";
+
+
+
+
+
+
+
+
+
+
+
+
+if (document.getElementById('resources-widget')) {
+  ReactDOM.render(<Resources />, document.getElementById('resources-widget'));
+}
