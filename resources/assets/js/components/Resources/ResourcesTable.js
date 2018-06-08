8e10646f4c61278f0e53ea227611e034f4e1da03
Jason Lipowicz <jason.lipowicz16@imperial.ac.uk>
2018-06-08 12:11:37 +0100

    Add further spacing on ResourcesTable widget

--- resources/assets/js/components/Resources/ResourcesTable.js
+++ resources/assets/js/components/Resources/ResourcesTable.js
@@ -5,17 +5,21 @@ import ResourcesRow from './ResourcesRow';
 
 export default class ResourcesTable extends Component {
   render() {
-    return ([
-      <div className="resources-table-header resources-row" key="resources-table-headers">
-        <div className="resources-table-cell">Name <img src="/images/icons8-sort-down-filled-50.png" /></div>
-        <div className="resources-table-cell">Type <img src="/images/icons8-sort-down-filled-50.png" /></div>
-        <div className="resources-table-cell">Downloads <img src="/images/icons8-sort-down-filled-50.png" /></div>
-        <div className="resources-table-cell">Uploaded <img src="/images/icons8-sort-down-filled-50.png" /></div>
-        <div className="resources-table-cell">Students <img src="/images/icons8-sort-down-filled-50.png" /></div>
-      </div>,
-      this.props.resources.map((r, i) =>
-        <ResourcesRow contacts={this.props.contacts} key={"resource"+i} resource={r} onAddStudent={this.props.onAddStudent} />
-      )
-    ]);
+    return (
+      <div className="resources-table">
+        <div className="resources-table-header resources-row" key="resources-table-headers">
+          <div className="resources-table-cell">Name <img src="/images/icons8-sort-down-filled-50.png" /></div>
+          <div className="resources-table-cell">Type <img src="/images/icons8-sort-down-filled-50.png" /></div>
+          <div className="resources-table-cell">Downloads <img src="/images/icons8-sort-down-filled-50.png" /></div>
+          <div className="resources-table-cell">Uploaded <img src="/images/icons8-sort-down-filled-50.png" /></div>
+          <div className="resources-table-cell">Students <img src="/images/icons8-sort-down-filled-50.png" /></div>
+        </div>
+        {
+          this.props.resources.map((r, i) =>
+            <ResourcesRow contacts={this.props.contacts} key={"resource"+i} resource={r} onAddStudent={this.props.onAddStudent} />
+          )
+        }
+      </div>
+    );
   }
 }
