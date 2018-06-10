0332cc28fc4cf02d3fff85eebfc3c8b9386ec365
Jason Lipowicz <jason.lipowicz16@imperial.ac.uk>
2018-06-10 15:43:50 +0100

    Further changes to sidebar for mobile

--- resources/views/dashboard/student.blade.php
+++ resources/views/dashboard/student.blade.php
@@ -4,21 +4,10 @@
 
 @section('content')
 
-<div class="header">
-  <div class="header-left">
-    <div class="header-icon">
-      <img class="logo-icon" src="/images/icon.png" />
-    </div>
-    <div class="page-title">Dashboard</div>
-    <div class="page-title page-dark">Welcome Jason!</div>
-  </div>
-  <div class="header-center">
-    <img src="images/logo.png" class="logo"/>
-  </div>
-  <div class="header-right">
-    <div class="header-logout"><a href="/logout"><img src="images/icons8-shutdown-50.png"/></a></div>
-  </div>
-</div>
+@component('header')
+  @slot('title') Dashboard @endslot
+  @slot('text') <div class="page-title page-dark">Welcome {{ explode(" ", Auth::user()->name)[0] }}</div> @endslot
+@endcomponent
 
 <div class="main">
   @component('sidebar', ["selected"=>"dashboard"])
--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,17 +1,18 @@
 <div class="header">
   <div class="header-left">
     <div class="header-icon">
-      <img class="logo-icon" src="/images/icon.png" />
+      <a href="/"><img class="logo-icon" src="/images/icon.png" /></a>
     </div>
     <div class="header-text">
       <div class="page-title">{{ $title }}</div>
       <div class="header-tabs">
         {{ $slot }}
       </div>
+      {{ $text ?? '' }}
     </div>
   </div>
   <div class="header-center">
-    <img src="/images/logo.png" class="logo" />
+    <a href="/"><img src="/images/logo.png" class="logo" /></a>
   </div>
   <div class="header-right">
     <div class="header-logout"><a href="/logout"><img src="/images/icons8-shutdown-50.png" /></a></div>
