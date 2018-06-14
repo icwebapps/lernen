57d479a79b48e955562803b2c2c6da021276efc2
Jason Lipowicz <jason.lipowicz16@imperial.ac.uk>
2018-06-14 01:07:12 +0100

    Completed Resources page with Sidebar

--- resources/assets/js/Pages/Resources.js
+++ resources/assets/js/Pages/Resources.js
@@ -5,6 +5,7 @@ import ResourcesUpload from '../Resources/ResourcesUpload';
 import ResourcesRow from '../Resources/ResourcesRow';
 import ResourcesTabSelector from '../Resources/ResourcesTabSelector';
 import SubjectSidebar from '../Resources/SubjectSidebar';
+import Sidebar from '../Widgets/Sidebar';
 
 export default class Resources extends Component {
   constructor() {
@@ -95,41 +96,45 @@ export default class Resources extends Component {
 
   render() {
     return ([
-      <div className="panel-subjects" key="panel-subjects">
-        <SubjectSidebar
-          selected={this.state.subject}
-          resources={this.state.resources}
-          subjects={this.state.subjects}
-          onChangeSubject={(subject)=>this.changeSubject(subject)}
-          hasAdd={this.state.addSubject}
-          onBeginAddSubject={(_)=>this.beginAddSubject()}
-          onAddSubject={()=>this.refreshResources()} />
-      </div>,
-      <div className="panel-resources" key="panel-resources">
-        <ResourcesTabSelector
-          key="resources-tab-selector"
-          selected={this.state.type}
-          subject={this.state.subject}
-          tabs={this.getTypes()}
-          onTabChange={(t)=>this.typeChange(t)} />
-        <ResourcesTable
-          key="resources-table"
-          subject={this.state.subject}
-          type={this.state.type}
-          resources={this.state.resources}
-          contacts={this.state.contacts}
-          onAddStudent={()=>this.refreshResources()} />
-        <ResourcesUpload
-          key="resources-upload"
-          onUpload={(new_type)=>this.onUpload(new_type)}
-          subject={this.state.subject}
-          type={this.state.type} />
+      <Sidebar selected={this.props.page} isTutor={this.props.isTutor} />,
+      <div className="width-fill">
+        <div className="panel-subjects" key="panel-subjects">
+          <SubjectSidebar
+            selected={this.state.subject}
+            resources={this.state.resources}
+            subjects={this.state.subjects}
+            onChangeSubject={(subject)=>this.changeSubject(subject)}
+            hasAdd={this.state.addSubject}
+            onBeginAddSubject={(_)=>this.beginAddSubject()}
+            onAddSubject={()=>this.refreshResources()} />
+        </div>
+        <div className="panel-resources" key="panel-resources">
+          <ResourcesTabSelector
+            key="resources-tab-selector"
+            selected={this.state.type}
+            subject={this.state.subject}
+            tabs={this.getTypes()}
+            onTabChange={(t)=>this.typeChange(t)} />
+          <ResourcesTable
+            key="resources-table"
+            subject={this.state.subject}
+            type={this.state.type}
+            resources={this.state.resources}
+            contacts={this.state.contacts}
+            onAddStudent={()=>this.refreshResources()} />
+          <ResourcesUpload
+            key="resources-upload"
+            onUpload={(new_type)=>this.onUpload(new_type)}
+            subject={this.state.subject}
+            type={this.state.type} />
+        </div>
       </div>
     ]);
   }
 }
 
 if (document.getElementById('resources-widget')) {
-    ReactDOM.render(<Resources />, document.getElementById('resources-widget'));
+  var el = document.getElementById('resources-widget')
+  ReactDOM.render(<Resources userId={el.dataset.userid} isTutor={el.dataset.istutor} page={el.dataset.page} />, el);
 }
 
