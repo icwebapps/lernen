ce6d1a1db212115ff42f748be6725175ddd3abc0
Jason Lipowicz <jason.lipowicz16@imperial.ac.uk>
2018-06-14 01:04:35 +0100

    Change Upcoming page to work with Sidebar

--- resources/assets/js/Pages/Upcoming.js
+++ resources/assets/js/Pages/Upcoming.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import ReactDOM from 'react-dom';
 import UpcomingColumn from '../Upcoming/UpcomingColumn';
+import Sidebar from '../Widgets/Sidebar';
 
 export default class Upcoming extends Component {
   constructor() {
@@ -20,20 +21,27 @@ export default class Upcoming extends Component {
 
   render() {
     let todayDate = new Date(this.state.today);
-    return (
-      [...Array(7)].map((_, i) => {
-        let thisDay = new Date(todayDate);
-        thisDay.setDate(todayDate.getDate() + i);
-        let thisEvents = this.state.events.filter(e => {
-          return e.date == thisDay.getDate() && e.month == thisDay.getMonth() + 1
-        });
-        return <UpcomingColumn key={"column-"+i} i={i} start={thisDay} events={thisEvents} />
-      })
-    );
+    return ([
+      <Sidebar selected={this.props.page} isTutor={this.props.isTutor} />,
+      <div className="width-fill flex-rows">
+        <div className="width-scrollable">
+        {
+          [...Array(7)].map((_, i) => {
+            let thisDay = new Date(todayDate);
+            thisDay.setDate(todayDate.getDate() + i);
+            let thisEvents = this.state.events.filter(e => {
+              return e.date == thisDay.getDate() && e.month == thisDay.getMonth() + 1
+            });
+            return <UpcomingColumn key={"column-"+i} i={i} start={thisDay} events={thisEvents} />
+          })
+        }
+        </div>
+      </div>
+    ]);
   }
-
 }
 
 if (document.getElementById('upcoming-widget')) {
-  ReactDOM.render(<Upcoming />, document.getElementById('upcoming-widget'));
+  var el = document.getElementById('upcoming-widget');
+  ReactDOM.render(<Upcoming userId={el.dataset.userid} isTutor={el.dataset.istutor} page={el.dataset.page} />, el);
 }
