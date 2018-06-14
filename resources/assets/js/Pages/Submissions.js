4a24d47a67f007b655d4e1c2b47a40f9839f470d
Alexander Zakon <azakon1997@gmail.com>
2018-06-14 22:26:58 +0100

    Displaying submitted solutions with two links: resource.url and submission.url

--- resources/assets/js/Pages/Submissions.js
+++ resources/assets/js/Pages/Submissions.js
@@ -7,30 +7,47 @@ export default class Submissions extends Component {
   constructor() {
     super();
     this.state = {
+      submissions: []
     };
+    this.loadSubmissions()
+  }
+
+  loadSubmissions() {
+    axios.get('/submissions/list').then((response) => {
+      this.setState(response.data);
+    });
   }
 
   render() {
     return ([
       <Sidebar key="sidebar" selected={this.props.page} isTutor={this.props.isTutor} />,
-      <div key="submissions-main" class="width-scrollable">
-        <div class="column">
-          <div class="column-title">Maths A-Level</div>
-          <div class="column-content">
-            <div class="card submission-card">
-              <div class="card-middle">
-                <div class="card-title"><a href="">Simplifying Fractions</a></div>
-                <div class="card-sub">
-                  Submitted on 10th June 2018 14:20<br />
-                  <a href="">simplifying_fractions_v1.pdf</a>
+      <div key="submissions-main" className="width-scrollable">
+        {
+          this.state.submissions.map((s) => {
+            return (
+              <div className="column">
+                <div className="column-title">Maths A-Level</div>
+                <div className="column-content">
+                  <div className="card submission-card">
+                    <div className="card-middle">
+                      <div className="card-title">
+                        <a href={s.assignment.resource.url} download>{s.assignment.title}</a>
+                      </div>
+                      <div className="card-sub">
+                        Submitted on 10th June 2018 14:20<br/>
+                        <a href={s.url}>{s.assignment.title + "_v1.pdf"}</a>
+                      </div>
+                    </div>
+                    <div className="card-right rating-excellent">
+                      <small>Grade</small>
+                      94%
+                    </div>
+                  </div>
                 </div>
               </div>
-              <div class="card-right rating-excellent">
-                <small>Grade</small>94%
-              </div>                
-            </div>
-          </div>
-        </div>
+            )
+          })
+        }
       </div>
     ]);
   }
