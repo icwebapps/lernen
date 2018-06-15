e24fa85bedc7d6d1254be9146b43bafa95b4b4af
Jason Lipowicz <jason.lipowicz16@imperial.ac.uk>
2018-06-15 16:17:12 +0100

    Scrolling pages shows plus icon

--- resources/assets/js/Pages/Feedback.js
+++ resources/assets/js/Pages/Feedback.js
@@ -7,12 +7,14 @@ export default class Feedback extends Component {
   constructor() {
     super();
     this.state = {
-      pages: []
+      pages: [],
+      hover: [null, null],
+      scroll: 0
     };
   }
 
   componentDidMount() {
-    this.loadPages();
+    this.loadPages();  
   }
 
   loadPages() {
@@ -21,15 +23,35 @@ export default class Feedback extends Component {
     });
   }
 
+  onHover(e, i) {
+    this.setState({ hover: [i, e.clientY + (this.state.scroll - 851*i) - 167] })
+  }
+
+  onLeave() {
+    this.setState({ hover: [null, null] });
+  }
+
+  handleScroll(event) {
+    let scrollTop = event.target.scrollTop;
+    this.setState({ scroll: scrollTop });
+  }
+
   render() {
     return ([
       <Sidebar key="sidebar" selected={this.props.page} isTutor={this.props.isTutor} />,
-      <div key="feeback-main" className="width-scrollable">
+      <div key="feeback-main" className="width-scrollable" style={{display: 'block'}} onScroll={(e)=>this.handleScroll(e)}>
         {
           this.state.pages.map((p, i) => {
             return (
-            <div key={"page"+i} className="feedback-page-item">
+            <div key={"page"+i} className="feedback-page-item" onMouseMove={(e)=>this.onHover(e, i)} onMouseLeave={()=>this.onLeave()}>
               <img src={"data:image/png;base64,"+p.data} />
+              <div className="feedback-annotate">
+              {
+                this.state.hover[0] == i ?
+                <img src="/images/icons8-plus-math-50.png" style={{top: Math.max(this.state.hover[1], 0)}} />
+                : null
+              }
+              </div>
             </div>
             )
           })
